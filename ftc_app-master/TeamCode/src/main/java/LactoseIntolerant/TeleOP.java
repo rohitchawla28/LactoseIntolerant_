45283b98ab2f99ba0b7aa7eca3ba04ef828a55e5
rohitchawla28 <31925602+rohitchawla28@users.noreply.github.com>
2019-02-06 21:23:47 -0600

    tele

--- ftc_app-master/TeamCode/src/main/java/LactoseIntolerant/TeleOP.java
+++ ftc_app-master/TeamCode/src/main/java/LactoseIntolerant/TeleOP.java
@@ -3,9 +3,12 @@ package LactoseIntolerant;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 @TeleOp
+        (name = "DownWithGluten", group = "TeleOp")
 public class TeleOP extends TeleLibs {
 
     public void loop() {
+        arcadeDrive();
 
     }
+
 }
