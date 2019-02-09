db1a09491617bb63e147f8a7b77995406225efe1
rohitchawla28 <31925602+rohitchawla28@users.noreply.github.com>
2019-02-09 00:15:16 -0600

    intake, output

--- ftc_app-master/TeamCode/src/main/java/LactoseIntolerantLibs/Intake.java
+++ ftc_app-master/TeamCode/src/main/java/LactoseIntolerantLibs/Intake.java
@@ -3,9 +3,9 @@ package LactoseIntolerantLibs;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
-
-import org.firstinspires.ftc.robotcore.external.Telemetry;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 public class Intake {
 
@@ -29,6 +29,64 @@ public class Intake {
 
         collectL = opMode.hardwareMap.crservo.get("collectL");
         collectR = opMode.hardwareMap.crservo.get("collectR");
+
+        intakeL.setDirection(DcMotorSimple.Direction.FORWARD);
+        intakeR.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        collectL.setDirection(DcMotorSimple.Direction.FORWARD);
+        collectR.setDirection(DcMotorSimple.Direction.REVERSE);
+
+    }
+
+    public void extend(double timeout) {
+        ElapsedTime time = new ElapsedTime();
+
+        while (time.seconds() < timeout && opMode.opModeIsActive()) {
+            intakeL.setPower(1);
+            intakeR.setPower(1);
+
+        }
+        intakeL.setPower(0);
+        intakeR.setPower(0);
+
+    }
+
+    public void retract(double timeout) {
+        ElapsedTime time = new ElapsedTime();
+
+        while (time.seconds() < timeout && opMode.opModeIsActive()) {
+            intakeL.setPower(-1);
+            intakeR.setPower(-1);
+
+        }
+        intakeL.setPower(0);
+        intakeR.setPower(0);
+
+    }
+
+    public void collect(double timeout) {
+        ElapsedTime time = new ElapsedTime();
+
+        while (time.seconds() < timeout && opMode.opModeIsActive()) {
+            collectL.setPower(1);
+            collectR.setPower(1);
+
+        }
+        collectL.setPower(0);
+        collectR.setPower(0);
+
+    }
+
+    public void spitOut(double timeout) {
+        ElapsedTime time = new ElapsedTime();
+
+        while (time.seconds() < timeout && opMode.opModeIsActive()) {
+            collectL.setPower(-0.7);
+            collectR.setPower(-0.7);
+
+        }
+        collectL.setPower(0);
+        collectR.setPower(0);
 
     }
 
--- ftc_app-master/TeamCode/src/main/java/LactoseIntolerantLibs/Output.java
+++ ftc_app-master/TeamCode/src/main/java/LactoseIntolerantLibs/Output.java
@@ -2,6 +2,9 @@ package LactoseIntolerantLibs;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 public class Output {
 
@@ -9,7 +12,45 @@ public class Output {
 
     private DcMotor output;
 
-    public Output (LinearOpMode opMode) {
-        
+    private Servo deposit;
+
+    public Output(LinearOpMode opMode) {
+        this.opMode = opMode;
+
+        output = opMode.hardwareMap.dcMotor.get("output");
+
+        deposit = opMode.hardwareMap.servo.get("deposit");
+
+        output.setDirection(DcMotorSimple.Direction.FORWARD);
+        output.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+
     }
+
+    public void extend(double timeout) {
+        ElapsedTime time = new ElapsedTime();
+
+        while (time.seconds() < timeout && opMode.opModeIsActive()) {
+            output.setPower(1);
+
+        }
+        output.setPower(0);
+
+    }
+
+    public void retract(double timeout) {
+        ElapsedTime time = new ElapsedTime();
+
+        while (time.seconds() < timeout && opMode.opModeIsActive()) {
+            output.setPower(-1);
+
+        }
+        output.setPower(0);
+
+    }
+
+    public void deposit() {
+        deposit.setPosition(0);
+
+    }
+
 }
