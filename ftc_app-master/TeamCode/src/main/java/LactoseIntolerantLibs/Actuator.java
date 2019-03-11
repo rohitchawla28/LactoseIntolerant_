d48ea88c38fbf8f2e3c4c2471493ac1762e15f7f
idevneo <idevneo@gmail.com>
2019-03-11 12:09:53 -0500

    Actuator Class

---
+++ ftc_app-master/TeamCode/src/main/java/LactoseIntolerantLibs/Actuator.java
@@ -0,0 +1,48 @@
+package LactoseIntolerantLibs;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+public class Actuator {
+
+    private LinearOpMode opMode;
+
+    private DcMotor actuator;
+
+    public Actuator(LinearOpMode opMode) {
+        this.opMode = opMode;
+
+        actuator = opMode.hardwareMap.dcMotor.get("actuator");
+
+
+        actuator.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+    }
+
+    public void hang(double power, double timeout) {
+        ElapsedTime time = new ElapsedTime();
+
+        while (time.seconds() < timeout && opMode.opModeIsActive()) {
+            actuator.setPower(power);
+
+        }
+        actuator.setPower(0);
+
+    }
+
+    public void dehang(double power, double timeout) {
+        ElapsedTime time = new ElapsedTime();
+
+        while (time.seconds() < timeout && opMode.opModeIsActive()) {
+            actuator.setPower(-power);
+
+        }
+        actuator.setPower(0);
+
+    }
+
+
+}
